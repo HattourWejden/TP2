a837564024b385c7a0a35910042f5eee2dd292e7
HattourWejden <wejden.hattour@eniso.u-sousse.tn>
2024-02-05 09:08:22 +0100

    tp2

---
+++ src/main/java/com/eniso/tp2/systemegestionetudiants/TP2SystemeGestionEtudiants.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.eniso.tp2.systemegestionetudiants;
+
+/**
+ *
+ * @author Hatto
+ */
+public class TP2SystemeGestionEtudiants {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
